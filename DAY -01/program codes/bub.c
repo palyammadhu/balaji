c65d030e092f17fa653e415bc46d7712c2faa1fb
balasri94 <129168327+balasri94@users.noreply.github.com>
2023-05-03 17:11:10 +0530

    Add files via upload

---
+++ DAY -01/program codes/bub.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+int i,j;
+int a[10]={2,34,54,456,87,90, 88,22,15,9};
+for(i=1;i<10;i++){
+for(j=0;j<10-i;j++){
+if(a[j]>a[j+1])
+{
+int temp=a[j];
+a[j]=a[j+1];
+a[j+1]=temp;
+}
+}
+}
+printf("\n");
+printf("ascending sorted elements\n");
+for(i=0;i<10;i++)
+printf("%d ",a[i]);}
