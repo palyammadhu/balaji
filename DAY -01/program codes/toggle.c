04efdfdf1f48847e238549914c70680c4448baf1
balasri94 <129168327+balasri94@users.noreply.github.com>
2023-05-04 10:53:33 +0530

    Update toggle.c

--- DAY -01/program codes/toggle.c
+++ DAY -01/program codes/toggle.c
@@ -8,7 +8,7 @@ for(i=31;i>=0;i--)
 printf("%d",n>>i&1);
 printf("\n");
 for(i=31;i>=0;i--)
-n=n^(1<<i);
+n=n^(1<<i);// it will toggle bits 1 means 0,0 means 1
 
 printf("after toggle \n");
 for(i=31;i>=0;i--)
